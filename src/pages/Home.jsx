d53a007244bdc494534da939a6554d7b6f15d725
HentaiSimpLord <pierrefrancois1907@gmail.com>
2025-07-08 01:43:33 +0200

    Content 2.0.1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,15 @@ import { Hero } from "../components/Hero";
 import { About } from "../components/About";
 import { Contact } from "../components/Contact";
 import { Footer } from "../components/Footer";
-import { CV } from "./CV";
 
 export const Home = () => {
   return (
     <>
-      <CV />
+      <Header />
+      <Hero />
+      <About />
+      <Contact />
+      <Footer />
     </>
   );
 };
