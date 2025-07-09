10992a9ce0020364d258abd6176132205c1f7275
HentaiSimpLord <pierrefrancois1907@gmail.com>
2025-07-09 13:34:26 +0200

    Style 4.0.2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import "../styles/Footer.scss";
+import githubLogo from "/src/assets/github.png";
+import impressumLogo from "/src/assets/agreement.png";
 
 export const Footer = () => {
   return (
@@ -8,10 +10,15 @@ export const Footer = () => {
           © {new Date().getFullYear()} Pierre François. Alle Rechte vorbehalten.
         </p>
         <a href="https://github.com/pierre-francois-06" target="_blank">
-          github
+          <img className="logo-link" src={githubLogo} alt="GITHUB.png" />
         </a>
         <a href="/impressum.html" target="_blank" rel="noopener noreferrer">
-          Impressum
+          <img
+            className="logo-link"
+            id="impressum-logo"
+            src={impressumLogo}
+            alt="IMPRESSUM.png"
+          />
         </a>
       </div>
     </section>
